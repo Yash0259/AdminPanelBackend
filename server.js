f9c7093c793e22a75ef780737b75e34469b7d01b
Yash Bhamre <89498904+Yash0259@users.noreply.github.com>
2025-03-22 12:11:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ app.use(cors({
 
 
 // Routes
-app.use('/', productRoutes);
-
+app.use("/products", productRoutes);
 app.get('/', (req, res) => {
     res.send("Hello, Node.js Backend is running!");
 });
